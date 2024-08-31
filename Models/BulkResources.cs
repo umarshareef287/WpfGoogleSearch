cf5b697324d71c61dc57b118e8d7a0cf2739058c
Umar Shareef <umarshareef287@gmail.com>
2024-08-31 21:06:44 +0300

    first commit

---
+++ Models/BulkResources.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class BulkResources
+    {
+        public static DataTable CreateEmptyDataTable()
+        {
+            DataTable dtableEmpData = new DataTable();
+            dtableEmpData.Columns.Add(new DataColumn("Id", typeof(int)));
+            dtableEmpData.Columns.Add(new DataColumn("WebTitle", typeof(string))); 
+            dtableEmpData.Columns.Add(new DataColumn("WebAddress", typeof(string))); 
+            dtableEmpData.Columns.Add(new DataColumn("SearchDescription", typeof(string))); 
+            
+            dtableEmpData.PrimaryKey = new DataColumn[] { dtableEmpData.Columns["Id"] };
+            return dtableEmpData;
+        }
+    }
+    
+}
